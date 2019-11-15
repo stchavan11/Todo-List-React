f5229f75c158d42064a2de303952a23e45235b7e
shubham chavan <stchavan.17@gmail.com>
2019-11-16 00:08:22 +0530

    Updates

---
+++ precache-manifest.99b26017413c36a9f2bd2df3881dda78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df94fd35f70686b424da63bb66974a75",
+    "url": "/Todo-List-React/index.html"
+  },
+  {
+    "revision": "3c821af3145b24c7d38b",
+    "url": "/Todo-List-React/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e6c17b9294900f6cdf4a",
+    "url": "/Todo-List-React/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3c821af3145b24c7d38b",
+    "url": "/Todo-List-React/static/js/2.d4dccb22.chunk.js"
+  },
+  {
+    "revision": "e6c17b9294900f6cdf4a",
+    "url": "/Todo-List-React/static/js/main.a86b00c7.chunk.js"
+  },
+  {
+    "revision": "9a1ec2ada81e3754107a",
+    "url": "/Todo-List-React/static/js/runtime-main.b6272ae9.js"
+  }
+]);
